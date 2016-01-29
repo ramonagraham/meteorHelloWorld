3a769987cb9f239b2e6f6098b9e2183ea518008a
ramonagraham <rgraham8@mail.greenriver.edu>
2016-01-29 14:55:38 -0800

    added descending button and css changes

--- helloWorld/helloWorld.js
+++ helloWorld/helloWorld.js
@@ -1,11 +1,23 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  
+  // counter starts at 10 to countdown
+  Session.setDefault('counterDescending', 10)
 
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
+	
+  });
+  
+   Template.helloDescending.helpers({
+  	
+	counterDescending: function () {
+      return Session.get('counterDescending');
+    }
+	
   });
 
   Template.hello.events({
@@ -13,7 +25,23 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
+	
+	
+	
   });
+  
+  
+  Template.helloDescending.events({
+  
+	
+	'click button': function () {
+      // decrement the counter when button is clicked
+      Session.set('counterDescending', Session.get('counterDescending') -1);
+    }
+	
+  });
+  
+  
 }
 
 if (Meteor.isServer) {
